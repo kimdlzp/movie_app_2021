5162307b9ea57ca977b9c9c58a015a53e5c1869b
KimSooYoung <kimdlzp@gmail.com>
2021-05-29 18:53:43 +0900

    practicing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from "prop-types";
 
-function App() {
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired
+};
+
+
+
+const foodILike = [
+  {
+    id:1,
+    name: "Salad",
+    image:
+      "https://simply-delicious-food.com/wp-content/uploads/2019/04/greek-chicken-salad-4.jpg"
+  },
+  {
+    id:2,
+    name: "Salmon",
+    image:
+      "https://www.eatwell101.com/wp-content/uploads/2020/12/Garlic-Butter-Baked-Salmon-recipe-2.jpg"
+  },
+  {
+    id:3,
+    name: "Sushi",
+    image:
+      "https://cdn.theculturetrip.com/wp-content/uploads/2016/04/ss_blackcod-1024x683.jpg"
+  },
+  {
+    id:4,
+    name: "Unagi",
+    image:
+      "https://www.natashathenomad.com/assets/images/cover/unagi.jpg"
+  }
+];
+
+
+function Food({ name, picture }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2> I Like {name}</h2>
+      <img src={picture} alt={name} />
     </div>
   );
 }
+
+function App() {
+  return (
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
+      ))}
+    </div>
+  );
+}
+
+
 
 export default App;
