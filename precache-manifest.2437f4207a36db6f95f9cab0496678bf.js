13bd3090fc1e165c1e469f51e70d2cd2ecd9a172
KimSooYoung <kimdlzp@gmail.com>
2021-05-30 22:22:31 +0900

    Updates

---
+++ precache-manifest.2437f4207a36db6f95f9cab0496678bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "444fc20c45ee215a1e63",
+    "url": "/movie_app_2021/static/css/main.170852cc.chunk.css"
+  },
+  {
+    "revision": "444fc20c45ee215a1e63",
+    "url": "/movie_app_2021/static/js/main.3c428aa2.chunk.js"
+  },
+  {
+    "revision": "2e0cce583920c9bddc4a",
+    "url": "/movie_app_2021/static/js/runtime~main.25bd41d2.js"
+  },
+  {
+    "revision": "31de54471d3e24ee8665",
+    "url": "/movie_app_2021/static/js/2.425b15d3.chunk.js"
+  },
+  {
+    "revision": "5747a6b2689a5933183134a52fc2bb16",
+    "url": "/movie_app_2021/index.html"
+  }
+];
